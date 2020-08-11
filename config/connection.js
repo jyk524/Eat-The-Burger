25b2abc3273e735ab6e056f6e22c843c504cac6a
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-08-10 20:21:46 -0400

    cleaned up code

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,6 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.DB_URL);
 }
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "Cbc2020!",
-//   database: "burgers_db",
-// });
-
 // Make connection.
 connection.connect(function (err) {
   if (err) {
